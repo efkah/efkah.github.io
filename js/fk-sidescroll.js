282de8f85a0a62656a0e519f1ef6664a1ee63490
MacBook von Felix <felix.s.krause@googlemail.com>
2019-08-14 05:16:48 +0200

    glitter

--- js/fk-sidescroll.js
+++ js/fk-sidescroll.js
@@ -1,26 +1,37 @@
 var Fk = Fk || {};
 Fk.SideScroll = new function () {
     let self = this;
-
+    this.viewWidth = (window.innerWidth > 980) ? 40 : 90 ;
     this.sections = document.querySelectorAll("section");
 
     this.selectArticleById = function (sectionId, articleId) {
         self.selectedSectionId = sectionId;
         let section = self.sections[sectionId]
-        var n = section.selectedArticle - articleId;
         i = 0;
         section.articles.forEach(article => {
             if (i < articleId) {
-                article.style.transform = `rotate(-5deg) translateX(${articleId * - 90}vw)`
-                // article.style.zIndex = 1;
+                console.log(i -articleId);
+                article.style.transform = `rotate(${(i - articleId) * 5}deg) translateX(${articleId * - self.viewWidth}vw)`;
+                article.style.backgroundColor = "#00000011";
+                article.style.borderRadius = "1vw";
+                // article.style.boxShadow = "0.1em 0.1em 0.4em #363020";
+                article.style.zIndex = 1400;
             } else if (i == articleId) {
-                article.style.transform = `translateX(${articleId * -90}vw)`
-                // article.style.zIndex = 1000;
+                console.log(i -articleId);
+                article.style.transform = `translateX(${articleId * -self.viewWidth}vw)`;
+                article.style.backgroundColor = "unset";
+                article.classList.remove("sidescroll-nav-hidden");
+                // article.style.boxShadow = "unset";
+                article.style.zIndex = 1500;
             } else {
-                article.style.transform = `rotate(5deg) translateX(${articleId * - 90}vw)`
-                // article.style.zIndex = 1;
+                console.log(i -articleId);
+                article.style.transform = `rotate(${(i -articleId) * 5}deg) translateX(${articleId * - self.viewWidth}vw)`;
+                article.style.backgroundColor = "#FFFFFF22";
+                article.style.borderRadius = "1vw";
+                article.classList.add("sidescroll-nav-hidden");
+                // article.style.boxShadow = "0.1em 0.1em 0.4em #363020";
+                article.style.zIndex = 1600;
             }
-            n++;
             i++;
         });
         section.selectedArticle = articleId;
@@ -58,13 +69,14 @@ Fk.SideScroll = new function () {
 
             section.articles.forEach(article => {
                 article.dataset.index = articleId;
-                article.style.transformOrigin = `${((articleId - 1) * - 90) - 20}vw ${articleId * - 5}vw`;
+                article.style.transformOrigin = `${((articleId - 1) * - self.viewWidth)}vw ${articleId * 5}vw`;
 
                 article.addEventListener("click", self.selectArticle, true);
                 if (articleId > 0) {
                     let backButton = document.createElement("div");
                     backButton.innerText = "<";
                     backButton.classList.add("back-button");
+                    backButton.classList.add("sidescroll-nav");
                     backButton.addEventListener("click", self.selectPreviousArticle, false);
                     article.appendChild(backButton);
                 }
@@ -72,18 +84,21 @@ Fk.SideScroll = new function () {
                     let nextButton = document.createElement("div");
                     nextButton.innerText = ">";
                     nextButton.classList.add("next-button");
+                    nextButton.classList.add("sidescroll-nav");
                     nextButton.addEventListener("click", self.selectNextArticle, false);
                     article.appendChild(nextButton);
                 }
                 articleId++
 
-            })
+            });
 
             self.selectArticleById(sectionId, 0);
-
+            let gridTemplateColumns = "";
             section.articles.forEach(article => {
+                gridTemplateColumns += self.viewWidth + "vw ";
                 article.style.transition = "all .3s ease-out";
             });
+            section.style.gridTemplateColumns = gridTemplateColumns;
             sectionId++
         });
     }
